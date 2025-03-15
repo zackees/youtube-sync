d1abcc982d3ff0ce913117aacacef7aed5e22a71
zackees <z@zackees.com>
2025-03-15 14:35:15 -0700

    test: add unit tests for library functionality

---
+++ tests/test_library.py
@@ -0,0 +1,17 @@
+"""
+Unit test file.
+"""
+
+import unittest
+
+
+class LibraryTester(unittest.TestCase):
+    """Main tester class."""
+
+    def test_simple(self) -> None:
+        """Test command line interface (CLI)."""
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
