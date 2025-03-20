05a42e7ae39e388cc946842126ae6a98f00fc688
zackees <z@zackees.com>
2025-03-20 15:30:35 -0700

    test: add integration tests for YouTube bot functionality

---
+++ tests/integration/test_youtube_bot.py
@@ -0,0 +1,38 @@
+# pylint: disable=all
+# flake8: noqa
+
+import unittest
+
+from youtube_sync.youtube.bot import (
+    VidEntry,
+    fetch_all_sources,
+    fetch_all_vids,
+    parse_youtube_videos,
+)
+
+URL_SILVERGURU = "https://www.youtube.com/@silverguru/videos"
+
+
+class YouTubeBotTester(unittest.TestCase):
+    def test_fetch_sources(self) -> None:
+        sources = list(fetch_all_sources(URL_SILVERGURU, limit=1))
+        print("sources:")
+        # for source in sources:
+        #    print(f"  {source}")
+        vids: list[VidEntry] = parse_youtube_videos(sources)
+        self.assertGreater(len(vids), 0)
+        print("vids:")
+        for vid in vids:
+            print(f"  {vid}")
+
+    def test_fetch_videos(self) -> None:
+        # vids = fetch_all_vids(URL_SILVERGURU)
+        sources: list[VidEntry] = fetch_all_vids(URL_SILVERGURU, limit=4)
+        print("vids:")
+        for vid in sources:
+            print(f"  {vid}")
+        self.assertGreater(len(sources), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
