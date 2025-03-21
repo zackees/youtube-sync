4e91d27a1241eeeda30c8d2c9537efec5e75c3c1
zackees <z@zackees.com>
2025-03-20 17:11:09 -0700

    refactor(api): streamline video scanning process in RumbleSyncImpl

--- src/youtube_sync/rumble/api.py
+++ src/youtube_sync/rumble/api.py
@@ -7,6 +7,7 @@ Command entry point.
 
 from youtube_sync.base_sync import BaseSync
 from youtube_sync.library import Library
+from youtube_sync.rumble.rumble import to_channel_url
 from youtube_sync.types import VidEntry
 
 
@@ -15,31 +16,17 @@ class RumbleSyncImpl(BaseSync):
         super().__init__(library, yt_dlp_uses_docker)
 
     def scan_for_vids(self, limit_scroll_pages: int | None) -> list[VidEntry]:
-        from youtube_sync.rumble.rumble_extra import rumble_scan
+        from youtube_sync.ytdlp_scan_for_vids import scan_for_vids
 
-        out: list[VidEntry] = rumble_scan(
-            channel_name=self.lib.channel_name,
-            limit_scroll_pages=limit_scroll_pages,
+        channel_name = self.lib.channel_name
+        channel_url = to_channel_url(channel_name)
+        stored_vids = self.lib.load()
+        full_scan = limit_scroll_pages is None
+        limit = limit_scroll_pages if limit_scroll_pages is not None else -1
+        out: list[VidEntry] = scan_for_vids(
+            channel_url=channel_url,
+            limit=limit,
+            stored_vids=stored_vids,
+            full_scan=full_scan,
         )
         return out
-
-
-# class BrighteonSyncImpl(BaseSync):
-#     def __init__(self, library: Library, yt_dlp_uses_docker: bool = False):
-#         super().__init__(library, yt_dlp_uses_docker)
-
-#     def scan_for_vids(self, limit_scroll_pages: int | None) -> list[VidEntry]:
-#         from youtube_sync.ytdlp_scan_for_vids import scan_for_vids
-
-#         channel_name = self.lib.channel_name
-#         channel_url = f"https://www.brighteon.com/channels/{channel_name}"
-#         stored_vids = self.lib.load()
-#         full_scan = limit_scroll_pages is None
-#         limit = limit_scroll_pages if limit_scroll_pages is not None else -1
-#         out: list[VidEntry] = scan_for_vids(
-#             channel_url=channel_url,
-#             limit=limit,
-#             stored_vids=stored_vids,
-#             full_scan=full_scan,
-#         )
-#         return out
