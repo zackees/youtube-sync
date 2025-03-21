f9113701918f190cca12d92d0e225ece0b088988
zackees <z@zackees.com>
2025-03-20 17:06:10 -0700

    feat: add audio download functionality using yt-dlp

--- src/youtube_sync/ytdlp_api.py
+++ src/youtube_sync/ytdlp_api.py
@@ -7,43 +7,6 @@ from pathlib import Path
 
 import yt_dlp
 
-URL = "https://www.youtube.com/watch?v=XfELJU1mRMg"
-
-
-def yt_dlp_vid_info(url: str) -> dict | Exception:
-    print("\n\n\n\n")
-    ydl_opts: dict = {
-        "verbose": True,
-        "--cookies-from-browser": ("chrome",),
-    }
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        try:
-            info = ydl.extract_info(url, download=False)
-            ydl.print_debug_header()
-            print("done")
-        except yt_dlp.utils.DownloadError as e:
-            return e
-    if not isinstance(info, dict):
-        return Exception(f"Expected dict, got {type(info)}")
-    return info
-
-
-def print_info(url: str) -> None:
-    # ℹ️ See help(yt_dlp.YoutubeDL) for a list of available options and public functions
-    # ydl_opts = {}
-    # with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    #     info = ydl.extract_info(url, download=False)
-
-    #     # ℹ️ ydl.sanitize_info makes the info json-serializable
-    #     print(json.dumps(ydl.sanitize_info(info)))
-
-    info_or_err = yt_dlp_vid_info(url)
-    if isinstance(info_or_err, Exception):
-        raise info_or_err
-    info = info_or_err
-    print(json.dumps(info, indent=2))
-
-
 def yt_dlp_download_audio(url: str, out_audio: Path) -> None:
     assert out_audio.suffix in [".mp3", ".m4a"]
     suffix = out_audio.suffix.replace(".", "")
@@ -63,5 +26,41 @@ def yt_dlp_download_audio(url: str, out_audio: Path) -> None:
         ydl.download([url])
 
 
-print_info(URL)
-# yt_dlp_download_audio(URL, Path("test.m4a"))
+
+
+def _dbg_yt_dlp_vid_info(url: str) -> dict | Exception:
+    print("\n\n\n\n")
+    ydl_opts: dict = {
+        "verbose": True,
+        "--cookies-from-browser": ("chrome",),
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        try:
+            info = ydl.extract_info(url, download=False)
+            ydl.print_debug_header()
+            print("done")
+        except yt_dlp.utils.DownloadError as e:
+            return e
+    if not isinstance(info, dict):
+        return Exception(f"Expected dict, got {type(info)}")
+    return info
+
+
+def _dbg_print_info(url: str) -> None:
+    # ℹ️ See help(yt_dlp.YoutubeDL) for a list of available options and public functions
+    # ydl_opts = {}
+    # with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    #     info = ydl.extract_info(url, download=False)
+
+    #     # ℹ️ ydl.sanitize_info makes the info json-serializable
+    #     print(json.dumps(ydl.sanitize_info(info)))
+
+    info_or_err = _dbg_yt_dlp_vid_info(url)
+    if isinstance(info_or_err, Exception):
+        raise info_or_err
+    info = info_or_err
+    print(json.dumps(info, indent=2))
+
+
+if __name__ == "__main__":
+    _dbg_print_info("https://www.youtube.com/watch?v=XfELJU1mRMg")
