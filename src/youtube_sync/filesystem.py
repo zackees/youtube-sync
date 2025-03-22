9770d084b713d98e46e84232f3fe41e2d7aaf57d
zackees <z@zackees.com>
2025-03-21 19:20:03 -0700

    update

--- src/youtube_sync/filesystem.py
+++ src/youtube_sync/filesystem.py
@@ -11,6 +11,18 @@ class FileSystem(abc.ABC):
     def copy(self, src: Path | str, dest: Path | str) -> None:
         pass
 
+    @abc.abstractmethod
+    def read_binary(self, path: Path | str) -> bytes:
+        pass
+
+    @abc.abstractmethod
+    def exists(self, path: Path | str) -> bool:
+        pass
+
+    def read_text(self, path: Path | str) -> str:
+        utf = self.read_binary(path)
+        return utf.decode("utf-8")
+
 
 class RealFileSystem(FileSystem):
     def __init__(self) -> None:
@@ -18,3 +30,10 @@ class RealFileSystem(FileSystem):
 
     def copy(self, src: Path | str, dest: Path | str) -> None:
         shutil.copy(str(src), str(dest))
+
+    def read_binary(self, path: Path | str) -> bytes:
+        with open(path, "rb") as f:
+            return f.read()
+
+    def exists(self, path: Path | str) -> bool:
+        return Path(path).exists()
