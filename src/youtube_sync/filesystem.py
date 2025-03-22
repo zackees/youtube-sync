d848ab71e2696b5ec97f355ff6354b8dec43c206
zackees <z@zackees.com>
2025-03-21 19:25:37 -0700

    update

--- src/youtube_sync/filesystem.py
+++ src/youtube_sync/filesystem.py
@@ -19,9 +19,17 @@ class FileSystem(abc.ABC):
     def exists(self, path: Path | str) -> bool:
         pass
 
+    @abc.abstractmethod
+    def write_binary(self, path: Path | str, data: bytes) -> None:
+        pass
+
     def read_text(self, path: Path | str) -> str:
         utf = self.read_binary(path)
         return utf.decode("utf-8")
+
+    def write_text(self, path: Path | str, data: str) -> None:
+        utf = data.encode("utf-8")
+        self.write_binary(path, utf)
 
 
 class RealFileSystem(FileSystem):
@@ -34,6 +42,10 @@ class RealFileSystem(FileSystem):
     def read_binary(self, path: Path | str) -> bytes:
         with open(path, "rb") as f:
             return f.read()
+
+    def write_binary(self, path: Path | str, data: bytes) -> None:
+        with open(path, "wb") as f:
+            f.write(data)
 
     def exists(self, path: Path | str) -> bool:
         return Path(path).exists()
@@ -52,3 +64,9 @@ class FSPath:
 
     def exists(self) -> bool:
         return self.fs.exists(self.path)
+
+    def __str__(self) -> str:
+        return self.path
+
+    def __repr__(self) -> str:
+        return f"FSPath({self.path})"
