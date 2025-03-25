ea850a03fb599aeaaa00dcec1be315ec59c2f26e
zackees <z@zackees.com>
2025-03-25 02:08:26 -0700

    feat: set DISPLAY environment variable for Xvfb

--- entrypoint.py
+++ entrypoint.py
@@ -6,6 +6,7 @@ PORT = str(os.environ.get("PORT", "80"))
 def main() -> None:
     """Main function."""
     proc_xfvb = subprocess.Popen(["Xvfb", ":1", "-screen", "0", "1024x768x24"])
+    os.environ["DISPLAY"] = ":1"
     proc = subprocess.Popen(["uv", "run", "-m", "http.server", PORT], cwd="www")
     with proc, proc_xfvb:
         subprocess.run(["uv", "run", "-m", "youtube_sync.cli.sync_multiple", "config.json"])
